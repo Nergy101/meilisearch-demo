ef6e30e0d2adb9664dc00cf4a5a2fd9688071c3b
Christian <chrisdvd@live.nl>
2023-06-19 21:50:00 +0200

    wip backend

--- src/backend/MeiliSearchDemo/MeiliSearchDemo/Program.cs
+++ src/backend/MeiliSearchDemo/MeiliSearchDemo/Program.cs
@@ -1,9 +1,16 @@
+using AutoFixture;
+using MeiliSearchDemo;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var serviceInstance = new SearchService<Movie>("movies");
 
 var app = builder.Build();
 
@@ -16,29 +23,17 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/search/{searchTerm}", (string searchTerm) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+
+    return serviceInstance.GetEntries(searchTerm, 100);
+
+}).WithOpenApi();
+
+app.MapPost("movies", async ([FromBody] IEnumerable<Movie> movies) =>
+{
+    await serviceInstance.AddEntries(movies);
+}).WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
